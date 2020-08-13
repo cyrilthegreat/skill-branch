6c966851f612c94d51a8edc7fd40f9aa739d6560
Mitrofanov.Kir <Kirill.Mitrofanov@infotecs.ru>
2020-08-13 19:48:37 +0300

    Add ViewModel & Nav

--- hometask_1/app/src/main/java/ru/skillbranch/gameofthrones/MainActivity.kt
+++ hometask_1/app/src/main/java/ru/skillbranch/gameofthrones/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.skillbranch.gameofthrones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
     }
 }
---
+++ hometask_1/app/src/main/java/ru/skillbranch/gameofthrones/MainViewModel.kt
@@ -0,0 +1,41 @@
+package ru.skillbranch.gameofthrones
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.skillbranch.gameofthrones.repositories.RootRepository
+
+class MainViewModel(val app: Application) : AndroidViewModel(app) {
+    private val repository = RootRepository
+    fun syncDataIfNeed() : LiveData<LoadResult<Boolean>> {
+        val result: MutableLiveData<LoadResult<Boolean>> = MutableLiveData(LoadResult.Loading(false))
+        viewModelScope.launch(Dispatchers.IO) {
+//            if (repository.isNeedUpdate()) {
+//                if (!app.applicationContext.is) {
+//
+//                }
+//                repository.sync()
+//                result.postValue(LoadResult.Sucess(true))
+//            } else {
+//                delay(5000)
+//                result.postValue(LoadResult.Sucess(true))
+//            }
+        }
+        return result
+    }
+}
+
+sealed class LoadResult<T> (
+    val data: T?,
+    val errorMsg: String? = null
+
+){
+    class Sucess<T>(data: T) : LoadResult<T>(data)
+    class Loading<T>(data: T? = null) : LoadResult<T>(data)
+    class Error<T>(msg: String, data: T? = null) : LoadResult<T>(data, msg)
+}
